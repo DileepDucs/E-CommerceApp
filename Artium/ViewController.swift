c1ce2aac6aee25f49f22f9b3925bda6bd107a864
Dileep Kumar Jaiswal <dileep.ducs@gmail.com>
2022-04-13 18:07:47 +0530

    Initial Commit

---
+++ Artium/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Artium
+//
+//  Created by Dileep Jaiswal on 13/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
